1e901445510140c9857399f88f444dd6adc1c6d5
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2023-11-06 12:23:01 +0100

    pepe jose

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const {Server} = require("socket.io");
 
-const io = new Server
+const io = new Server(3000);
 
 io.on("connection", (socket) => {
+  console.log("ngrrwfsngbuefsgjs<i")
   socket.emit("hello", "world");
+
+  socket.on("EnviarCodiPeli", (args) => {
+    console.log(args);
+  })
 
   socket.on("howdy", (arg) => {
     console.log(arg);
