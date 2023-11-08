46b4ec0fc34ee2921ea38fcc04026c200e911319
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2023-11-08 09:04:11 +0100

    Sockets work Made by Isma

--- src/server.js
+++ src/server.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const {Server} = require("socket.io");
+const port = 3000;
+const io = new Server(8888);
+const app = express();
 
-const io = new Server(3000);
+const cors = require('cors');
+
+//App server can't listen to the same port of sockets
+
+app.use(cors());
+app.use(express.json());
+app.listen(port, () => {
+  console.log(`El servidor està escoltant el port::${port}`);
+})
 
 io.on("connection", (socket) => {
   console.log("ngrrwfsngbuefsgjs<i")
   socket.emit("hello", "world");
 
-  socket.on("EnviarCodiPeli", (args) => {
+  socket.on("RequestCodiPeli", (args) => {
+    socket.emit("CodiPeli", "XDXDXDXD")
     console.log(args);
   })
 
   socket.on("howdy", (arg) => {
     console.log(arg);
+  })
+
+  socket.on("disconnect", (reason) => {
+    console.log(`socket ${socket.id} disconnected due to ${reason}`);
   })
 })
