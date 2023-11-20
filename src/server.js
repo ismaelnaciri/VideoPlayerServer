d25dd9d422508633084d5aa30125c1a0a131297d
ismaelnaciri <114733902+ismaelnaciri@users.noreply.github.com>
2023-11-20 12:08:28 +0100

    major upgrades isma

--- src/server.js
+++ src/server.js
@@ -24,8 +24,9 @@ files.forEach(element => {
     || element.split('.')[1] === 'ogg') {
 
       videos.push({
-        title: element,
-        url: "assets/".concat(element)
+        title: element.split('.')[0].replace('_', " ").split('720p')[0],
+        url: "assets/".concat(element),
+        opened: false
       });
   }
 })
@@ -49,24 +50,43 @@ io.on("connection", (socket) => {
     socket.emit("VideoList", videos);
   })
 
-  socket.on("RequestCodiVideo", (args) => {
-    socket.emit("CodiVideo", "XDXDXDXD")
+  //First angular client calls requestCodiVideo
+  socket.on("RequestVideoVerification", (args) => {
     console.log(args);
-  })
 
-  socket.on("EnviarCodiPeli", (args) => {
-    console.log(args);
+    let codi = generateRandomString();
+    socket.emit("CodiVideo", codi)
+
+    socket.on("EnviarCodiPeli", (androidCodi) => {
+      console.log(androidCodi);
+      if (codi === androidCodi)
+        socket.emit("VeifiedCorrectly", true);
+    })
   })
 
   socket.on("howdy", (arg) => {
     console.log(arg);
   })
 
-  socket.on("disconnect", (reason) => {
-    console.log(`socket ${socket.id} disconnected due to ${reason}`);
-  })
+  // socket.on("disconnect", (reason) => {
+  //   console.log(`socket ${socket.id} disconnected due to ${reason}`);
+  // })
 
   //crear array d'objectes
   //Object fields: title, url
   //format of the url is "assets/video.mp4"
 })
+
+
+function generateRandomString() {
+  const characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let randomString = '';
+
+  for (let i = 0; i < 4; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+
+  return randomString;
+}
+
